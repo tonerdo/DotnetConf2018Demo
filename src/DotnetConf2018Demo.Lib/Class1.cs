bffa38bae338b176e5798199e44febbfe5a74b83
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-09-13 00:16:28 +0100

    add Math class

--- src/DotnetConf2018Demo.Lib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DotnetConf2018Demo.Lib
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/DotnetConf2018Demo.Lib/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotnetConf2018Demo.Lib
+{
+    public class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
