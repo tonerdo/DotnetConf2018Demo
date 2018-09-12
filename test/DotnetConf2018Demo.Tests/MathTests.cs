e596d42df4c93cf5edebd77ab9230ca56db53ae0
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-09-13 00:26:30 +0100

    scaffold unit test project

---
+++ test/DotnetConf2018Demo.Tests/MathTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DotnetConf2018Demo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
