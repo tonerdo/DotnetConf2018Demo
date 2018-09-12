c05f5808cf675b439ea3277f0114a47fae38dedf
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-09-13 00:33:37 +0100

    add tests for Math public methods

--- test/DotnetConf2018Demo.Tests/MathTests.cs
+++ test/DotnetConf2018Demo.Tests/MathTests.cs
@@ -1,4 +1,4 @@
-using System;
+using DotnetConf2018Demo.Lib;
 using Xunit;
 
 namespace DotnetConf2018Demo.Tests
@@ -6,9 +6,21 @@ namespace DotnetConf2018Demo.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestAdd()
         {
-            
+            Assert.Equal(Math.Add(10, 10), 20);
+        }
+
+        [Fact]
+        public void TestSub()
+        {
+            Assert.Equal(Math.Sub(10, 10), 0);
+        }
+
+        [Fact]
+        public void TestMul()
+        {
+            Assert.Equal(Math.Mul(10, 10), 100);
         }
     }
 }
